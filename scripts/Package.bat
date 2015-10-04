1be428762d49749c39c2bb93282b01b8232efb07
Adan <Adan@Adan-PC>
2015-10-04 18:21:46 +0200

    Ligia Cano

---
+++ scripts/Package.bat
@@ -0,0 +1,22 @@
+@echo off
+set workspace=C:\Users\Adan\workspace\ligia.cano.ecp1.miw.upm.es
+set PATH=%PATH%C:\Program Files\Java\jdk1.8.0_60\bin;C:\Users\Adan\Downloads\apache-maven-3.3.3-bin\apache-maven-3.3.3\bin
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_60
+set M2_HOME=C:\Users\Adan\Downloads\apache-maven-3.3.3-bin\apache-maven-3.3.3
+echo -----------------------------------------
+echo . (C) MIW
+echo -----------------------------------------
+echo .
+echo Workspace --- %workspace%
+echo .
+cd %workspace%
+echo ============ mvn --version =======================================================
+echo .
+
+call mvn clean 
+
+call mvn test 
+
+call mvn package -Denvironment.type=preproduction
+
+
